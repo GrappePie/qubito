cf4eb6feff64a5e14c1b2c56ba7651ab523646b5
RandyRandomx86 <randy2348.rj@gmail.com>
2025-07-14 19:01:58 -0600

    UI - Add Create Item Page

--- src/components/item/createItem.tsx
+++ src/components/item/createItem.tsx
@@ -1,6 +1,10 @@
 "use client";
 import {useForm} from "@/hooks/useForm";
 import {Item} from "@/models/Item";
+import { Input, Select, Radio, Switch, Space, InputNumber, Card} from "antd";
+import ImageInput from "@/components/item/ImageInput";
+const { TextArea } = Input;
+
 
 const initialItem: Item = {
     id: "",
@@ -16,16 +20,87 @@ const initialItem: Item = {
     isAvailableForSale: false,
     lowStock: 0,
     owner: "",
-    sku: 0,
+    sku: "",
     variants: []
 };
+
 const CreateItem = () => {
-    const {name, handleInputChange} = useForm<Item>(initialItem);
+    const {name,categories,isAvailableForSale,price,cost,sku,barCode, handleInputChange} = useForm<Item>(initialItem);
+    
     return (
-        <div className="container w-2/3 mx-auto">
-            <div className="card w-full rounded-lg overflow-hidden shadow-lg bg-white">
-                <h2 className="text-center">Create New Item</h2>
-            </div>
+        <div className="container grid grid-cols-1 gap-10 w-2/3 h-[90vh] overflow-y-scroll hide-scrollbar mx-auto">
+            <Card title="Create New Item" className="z-0 shadow-lg bg-white">
+                <div className="grid grid-cols-2 gap-15 gap-y-8 p-4">
+                    <div className="col-span-2">
+                        <ImageInput />
+                    </div>
+                    <Input type="text" name="name" onChange={handleInputChange} defaultValue={name} placeholder="Product Name" required/>
+                    <Select
+                        placeholder={"Product Categories"}
+                        mode="multiple"
+                        value={categories}
+                        onChange={(value) => handleInputChange({ name: "categories", value })}
+                        options={[
+                            { value: 'Forniture', label: 'Forniture' },
+                            { value: 'Chamyto Category', label: 'Chamyto Category' },
+                            { value: 'Yiminghe', label: 'yiminghe' },
+                            { value: 'disabled', label: 'Disabled', disabled: true },
+                        ]}
+                        className="w-full"
+                    />
+                    <TextArea rootClassName="col-span-2" rows={4} placeholder="Description" showCount maxLength={100} />
+                    <div>
+                        <span className="mr-4">Available for sale.</span>
+                        <Switch defaultChecked value={isAvailableForSale} onChange={(value) => handleInputChange({name:"isAvailableForSale" ,value })} />
+                    </div>
+                    <div>
+                        <span className="mr-4">Sold By</span><Radio.Group
+                        value={1}
+                        options={[
+                            { value: 1, label: 'Weight / Volume' },
+                            { value: 2, label: 'Each' },
+                        ]}
+                    /></div>
+                    <Space className="col-span-2">
+                        <span className="mr-4">Price</span>
+                        <InputNumber<number>
+                            name="price"
+                            defaultValue={price}
+                            formatter={(value) => `$ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+                            parser={(value) => value?.replace(/\$\s?|(,*)/g, '') as unknown as number}
+                            onChange={(value)=> handleInputChange({name:"cost",value:value ?? 0})}
+                        />
+                        <span className="mr-4">Price</span>
+                        <InputNumber<number>
+                            name="cost"
+                            defaultValue={cost}
+                            formatter={(value) => `$ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+                            parser={(value) => value?.replace(/\$\s?|(,*)/g, '') as unknown as number}
+                            onChange={(value)=> handleInputChange({name:"cost",value:value ?? 0})}
+                        />
+                    </Space>
+                    <Input
+                        type="text"
+                        name="sku"
+                        onChange={handleInputChange}
+                        value={sku}
+                        placeholder="SKU" />
+                    <Input
+                        type="text"
+                        name="barCode"
+                        onChange={handleInputChange}
+                        value={barCode}
+                        placeholder="Bar Code" />
+                </div>
+            </Card>
+            <Card title="Inventory" className="shadow-lg bg-white mt-4">
+                
+            </Card>
+            <Card title="Variants" className="shadow-lg bg-white mt-4">
+                
+            </Card>
+            <Card title="Modifiers" className="shadow-lg bg-white mt-4">
+            </Card>
         </div>
     );
 };
