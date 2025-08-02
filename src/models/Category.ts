043abd6b5d1156b5b249f79eba2156abd584219b
RandyRandomx86 <randy2348.rj@gmail.com>
2025-08-02 12:21:24 -0600

    Mongo - Add Category and Item Schema

---
+++ src/models/Category.ts
@@ -0,0 +1,26 @@
+import {Item} from "@/models/Item";
+import mongoose, {Model} from 'mongoose';
+
+export interface ICategory {
+    categoryId: string;
+    name: string;
+    description: string;
+    imageUrl: string;
+    parentCategoryId?: string;
+    isActive: boolean;
+    products: Partial<Item>[]; // Array of products in this category
+}
+
+// Define the Category schema
+const CategorySchema = new mongoose.Schema<ICategory>({
+    categoryId: {type: String, required: true, unique: true},
+    name: {type: String, required: true},
+    description: {type: String, required: true},
+    imageUrl: {type: String, required: true},
+    parentCategoryId: {type: String, default: null},
+    isActive: {type: Boolean, default: true},
+    products: [{type: mongoose.Schema.Types.ObjectId, ref: 'Item'}]
+}, {timestamps: true});
+
+const CategoryModel: Model<ICategory> = mongoose.models.Category || mongoose.model<ICategory>('Category', CategorySchema);
+export default CategoryModel;
