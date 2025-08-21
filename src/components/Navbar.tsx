124a66a1912d823b1374c1cf6b1cc60f122a2fbf
RandyRandomx86 <randy2348.rj@gmail.com>
2025-08-20 21:49:09 -0600

    Style - Improve basted on mockup

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,27 +1,109 @@
 import Link from "next/link";
-import Image from "next/image";
+
 
 export default function Navbar() {
     return (
-        <aside className="w-64 bg-blue-900 text-white min-h-screen p-4">
-            <h2 className="text-xl font-semibold mb-6">
-                <Image src="/Qubito-icon.svg" width={24} height={24} alt="Qubito Icon" className="inline-block mr-2" />
-                Qubito
-            </h2>
-            <nav className="space-y-2">
-                <Link href="/inventory" className="block py-2 px-4 rounded hover:bg-blue-800">
-                    Inventory
+        <aside className="w-64 bg-slate-800 text-white min-h-screen p-4">
+            <div className="text-xl font-semibold mb-6 flex items-center border-b border-slate-700 pb-2">
+                <svg className="h-8 w-8 text-sky-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                     stroke-width="1.5" stroke="currentColor">
+                    <path strokeLinecap="round" stroke-linejoin="round"
+                          d="M21 7.5l-9-5.25L3 7.5m18 0l-9 5.25m9-5.25v9l-9 5.25M3 7.5l9 5.25M3 7.5v9l9 5.25m0-9v9"></path>
+                </svg>
+                <span className="ml-3 text-2xl font-bold">Qubito POS</span>
+            </div>
+            <div className="space-y-2">
+                <Link href="#" className="nav-link bg-slate-700 px-4 py-3 rounded-lg flex gap-2">
+                    <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                         stroke-width="1.5" stroke="currentColor">
+                        <path strokeLinecap="round" stroke-linejoin="round"
+                              d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"></path>
+                    </svg>
+                    <span>Mesas</span>
                 </Link>
-                <Link href="/pos" className="block py-2 px-4 rounded hover:bg-blue-800">
-                    Punto de Venta
+                <Link href="#" className="px-4 py-3 rounded-lg flex gap-2">
+                    <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                         stroke-width="1.5" stroke="currentColor">
+                        <path strokeLinecap="round" stroke-linejoin="round"
+                              d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c.51 0 .962-.344 1.087-.835l1.823-6.84a2.25 2.25 0 00-2.168-2.618H6.364a2.25 2.25 0 00-2.168 2.618l1.823 6.84zM7.5 14.25h11.218M7.5 14.25a3 3 0 01-3-3h.008v.008h-.008v-.008zm0 0h.008v.008h-.008v-.008zm7.5 0a3 3 0 01-3-3h.008v.008h-.008v-.008zm0 0h.008v.008h-.008v-.008z"></path>
+                    </svg>
+                    <span>Venta rápida</span>
                 </Link>
-                <Link href="/notifications" className="block py-2 px-4 rounded hover:bg-blue-800">
-                    Notifications
+                <Link href="#" className="px-4 py-3 rounded-lg flex gap-2">
+                    <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                         stroke-width="1.5" stroke="currentColor">
+                        <path strokeLinecap="round" stroke-linejoin="round"
+                              d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"></path>
+                    </svg>
+                    <span>Corte de caja</span>
                 </Link>
-                <Link href="/test" className="block py-2 px-4 rounded hover:bg-blue-800">
-                    Test Pages
+                <Link href="#" className="px-4 py-3 rounded-lg flex gap-2">
+                    <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                         stroke-width="1.5" stroke="currentColor">
+                        <path strokeLinecap="round" stroke-linejoin="round"
+                              d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25"></path>
+                    </svg>
+                    <span>Historial de ventas</span>
                 </Link>
-            </nav>
+                <hr className="border-slate-700 my-2"/>
+                <Link href="#" className="px-4 py-3 rounded-lg flex gap-2">
+                    <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                         stroke-width="1.5" stroke="currentColor">
+                        <path strokeLinecap="round" stroke-linejoin="round"
+                              d="M3.75 6A2.25 2.25 0 016 3.75h2.25A2.25 2.25 0 0110.5 6v2.25a2.25 2.25 0 01-2.25 2.25H6a2.25 2.25 0 01-2.25-2.25V6zM3.75 15.75A2.25 2.25 0 016 13.5h2.25a2.25 2.25 0 012.25 2.25V18a2.25 2.25 0 01-2.25 2.25H6A2.25 2.25 0 013.75 18v-2.25zM13.5 6a2.25 2.25 0 012.25-2.25H18A2.25 2.25 0 0120.25 6v2.25A2.25 2.25 0 0118 10.5h-2.25a2.25 2.25 0 01-2.25-2.25V6zM13.5 15.75a2.25 2.25 0 012.25-2.25H18a2.25 2.25 0 012.25 2.25V18A2.25 2.25 0 0118 20.25h-2.25A2.25 2.25 0 0113.5 18v-2.25z"></path>
+                    </svg>
+                    <span>Dashboard</span>
+                </Link>
+                <Link href="#" className="px-4 py-3 rounded-lg flex gap-2">
+                    <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                         stroke-width="1.5" stroke="currentColor">
+                        <path strokeLinecap="round" stroke-linejoin="round"
+                              d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 013 19.875v-6.75zM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V8.625zM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V4.125z"></path>
+                    </svg>
+                    <span>Reportes</span>
+                </Link>
+                <Link href="#" className="px-4 py-3 rounded-lg flex gap-2">
+                    <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                         stroke-width="1.5" stroke="currentColor">
+                        <path strokeLinecap="round" stroke-linejoin="round"
+                              d="M3.75 12h16.5m-16.5 3.75h16.5M3.75 19.5h16.5M5.625 4.5h12.75a1.875 1.875 0 010 3.75H5.625a1.875 1.875 0 010-3.75z"></path>
+                    </svg>
+                    <span>Productos</span>
+                </Link>
+                <Link href="/inventory" className="px-4 py-3 rounded-lg flex gap-2">
+                    <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                         stroke-width="1.5" stroke="currentColor">
+                        <path strokeLinecap="round" stroke-linejoin="round"
+                              d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z"></path>
+                    </svg>
+                    <span>Inventario</span>
+                </Link>
+                <Link href="#" className="px-4 py-3 rounded-lg flex gap-2">
+                    <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                         stroke-width="1.5" stroke="currentColor">
+                        <path strokeLinecap="round" stroke-linejoin="round"
+                              d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-4.663M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0z"></path>
+                    </svg>
+                    <span>Clientes</span>
+                </Link>
+                <Link href="#" className="px-4 py-3 rounded-lg flex gap-2">
+                    <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                         stroke-width="1.5" stroke="currentColor">
+                        <path strokeLinecap="round" stroke-linejoin="round"
+                              d="M8.25 18.75a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 01-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h1.125c.621 0 1.125-.504 1.125-1.125V14.25m-17.25 4.5v-1.875a3.375 3.375 0 013.375-3.375h9.75a3.375 3.375 0 013.375 3.375v1.875m-17.25 4.5h16.5M6 6.75h.008v.008H6V6.75z"></path>
+                    </svg>
+                    <span>Proveedores</span>
+                </Link>
+                <Link href="/pos" className="nav-link px-4 py-3 rounded-lg flex gap-2">
+                    <span>Punto de Venta</span>
+                </Link>
+                <Link href="/notifications" className="nav-link px-4 py-3 rounded-lg flex gap-2">
+                    <span>Notifications</span>
+                </Link>
+                <Link href="/test" className="nav-link px-4 py-3 rounded-lg flex gap-2">
+                    <span>Test</span>
+                </Link>
+            </div>
         </aside>
     );
 }
