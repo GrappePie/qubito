4e1c86663be7ab091e1cd5a6fa3a271834d2843e
RandyRandomx86 <randy2348.rj@gmail.com>
2025-08-22 20:42:58 -0600

    Pages - Add page tables

--- src/app/tables/page.tsx
+++ src/app/tables/page.tsx
@@ -1,9 +1,15 @@
+import TitlePage from "@/components/common/TitlePage";
+import TablesComponent from "@/components/tables/TablesComponent";
 
-
+const tables = Array.from({ length: 20 }, (_, i) => ({
+    number: i + 1,
+    total: i == 11 ? 12.5 : 0,
+}));
 const Tables = () => {
     return (
         <div>
-            <h1>Mesas</h1>
+            <TitlePage title="Gestión de Mesas" subtitle={"Selecciona una mesa para iniciar o continuar una orden."}/>
+            <TablesComponent tables={tables} />
         </div>
     );
 };
