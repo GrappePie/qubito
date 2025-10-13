5f20497459dff81fc23650739d59844c4c568bec
RandyRandomx86 <randy2348.rj@gmail.com>
2025-10-12 19:42:56 -0600

    UI - Add categories container in sale

--- src/components/sale/SaleContainer.tsx
+++ src/components/sale/SaleContainer.tsx
@@ -5,7 +5,7 @@ import ProductsContainer from "@/components/sale/ProductsContainer";
 
 const SaleContainer = () => {
     return (
-        <div className={"w-full h-full min-h-0"}>
+        <div className={"w-full h-full min-h-0 flex flex-col"}>
             {/* Search bar container */}
             <div className={"w-full h-16 flex p-2"}>
                 {/* Search bar */}
@@ -18,8 +18,22 @@ const SaleContainer = () => {
                     </button>
                 </div>
             </div>
+            {/*Categories container*/}
+            <div className={"w-full relative h-10 px-2 py-1"}>
+                <div className={"w-full min-w-0 h-full flex flex-nowrap px-4 overflow-x-auto overflow-y-hidden"}>
+                    {/* Categories */}
+                    {Array.from({ length: 10 }, (x, index) => (
+                        <div
+                            key={index}
+                            className={"min-w-max bg-slate-300 text-slate-800 font-semibold px-4 rounded-lg flex items-center justify-center mr-2 hover:bg-slate-400 transition-colors whitespace-nowrap"}
+                        >
+                            Categoría {index + 1}
+                        </div>
+                    ))}
+                </div>
+            </div>
             {/* Products container */}
-            <div className={"w-full h-[calc(100%-4rem)] p-2 overflow-y-auto"}>
+            <div className={"flex-1 m-2 overflow-y-auto"}>
                 <ProductsContainer/>
             </div>
         </div>
